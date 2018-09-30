3756982a6c63bd0c6f67d39099adb31eef341961
George  Pianka <gpianka1@gmail.com>
2018-09-30 04:43:52 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,9 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
-("Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
-("Rowdy", 10, "M", "black", "intense", 1),
+("Mr. Chocolate", 20, "M", "Dark Brown", "calm", 0),
+("Rowdy", 10, "F", "Black", "intense", 1),
+("Tabitha", 6, "F", "Dark Brown", "Nice", 1),
+("Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+("Melissa", 13, "F", "Black", "goofy", 1),
+("Grinch", 2, "M", "Black", "Grinchy", 1),
+("Wendy", 6, "F", "Blue", "naive", 1),
+(NULL, 20, "M", "Brown", "aggressive", 0);
